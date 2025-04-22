15a91ccf434890db48b1ca0f5b8b812857925f90
Asadullah Al Galib <aagalib2323@gmail.com>
2025-04-22 22:48:00 +0600

    macros

---
+++ 8_macros/macro.asm
@@ -0,0 +1,38 @@
+section .data
+  digit db 0, 10
+  
+section .text
+  global _start
+  
+;exit macro
+%macro exit 0 ;0 is the argc
+  mov rax, 60
+  mov rdi, 0
+  syscall
+%endmacro
+
+;print digit macro
+%macro printDigit 1 ;1 is argc
+  mov rax, %1
+  call _printRAXDigit
+%endmacro
+
+
+_start:
+  
+  printDigit 3
+  printDigit 4
+
+  exit
+
+
+_printRAXDigit: ;lets print 0
+  add rax, 48 ;ascii of 0 is 48
+  mov [digit], al ;mov the lower byte in digit
+
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, digit
+  mov rdx, 2 ;newline and digit two bits
+  syscall
+  ret
